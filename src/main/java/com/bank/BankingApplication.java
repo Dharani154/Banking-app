ae011dd5da28b34d9e47a9ab69c92c3fc29ebf72
Dharani154 <dharanigilli1510@gmail.com>
2026-06-08 06:18:30 +0000

    Initial commit

---
+++ src/main/java/com/bank/BankingApplication.java
@@ -0,0 +1,23 @@
+package com.bank;
+
+public class BankingApplication {
+
+    public static void main(String[] args) {
+
+        double balance = 10000;
+
+        balance = deposit(balance,5000);
+
+        balance = withdraw(balance,2000);
+
+        System.out.println("Current Balance: " + balance);
+    }
+
+    public static double deposit(double balance,double amount){
+        return balance + amount;
+    }
+
+    public static double withdraw(double balance,double amount){
+        return balance - amount;
+    }
+}
